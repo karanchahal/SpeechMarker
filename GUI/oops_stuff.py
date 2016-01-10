7b511871b61946c5a6dcde327adecc6b29bae8b7
Karanbir Chahal <karanbleep@gmail.com>
2016-01-10 16:26:26 +0530

    Button functions

--- GUI/oops_stuff.py
+++ GUI/oops_stuff.py
@@ -3,21 +3,25 @@ from PyQt4 import QtGui,QtCore # QtCore for even Handling
 
 class  Window(QtGui.QMainWindow):   # Inheriting
 
-     def __init__(self):
-         super(Window,self).__init__() # with super we return a parent object,overides the default constructor
-         self.setGeometry(50,50,500,300)
-         self.setWindowTitle("PyQT")
-         self.setWindowIcon(QtGui.QIcon('icons/icon.png'))
-         self.home()
+    def __init__(self):
+        super(Window,self).__init__() # with super we return a parent object,overides the default constructor
+        self.setGeometry(50,50,500,300)
+        self.setWindowTitle("PyQT")
+        self.setWindowIcon(QtGui.QIcon('icons/icon.png'))
+        self.home()
 
-     def home(self):
-         btn = QtGui.QPushButton("Quit",self) # Quits the application button
-         btn.clicked.connect(QtCore.QCoreApplication.instance().quit)
+    def home(self):
+        btn = QtGui.QPushButton("Quit",self) # Quits the application button
+        btn.clicked.connect(self.close_application)
 
-         btn.resize(100,100)
-         btn.move(100,100)
+        btn.resize(btn.sizeHint()) # or btn.minimumSizeHint()
+        btn.move(100,100)
 
-         self.show()
+        self.show()
+
+    def close_application(self):
+        print("Customised exit")
+        sys.exit()
 
 def run():
     app = QtGui.QApplication(sys.argv)
