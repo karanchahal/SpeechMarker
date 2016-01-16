52ae38d7f45602c72b5630e25df10ca4154c285a
Karanbir Chahal <karanbleep@gmail.com>
2016-01-16 18:03:43 +0530

    Added progress bar support

--- GUI/oops_stuff.py
+++ GUI/oops_stuff.py
@@ -24,7 +24,7 @@ class  Window(QtGui.QMainWindow):   # Inheriting
         self.home()
 
     def home(self):
-        '''Adding Buttons '''
+        ''' Adding Buttons '''
         btn = QtGui.QPushButton("Quit",self) # Quits the application button
         btn.clicked.connect(self.close_application)
 
@@ -32,22 +32,37 @@ class  Window(QtGui.QMainWindow):   # Inheriting
         btn.move(100,100)
 
 
-        '''Connects the Action to the yet to be done toolbar'''
+        ''' Connects the Action to the yet to be done toolbar '''
         extractAction = QtGui.QAction(QtGui.QIcon('icons/icon.png'),'Flee the Scene',self)
         extractAction.triggered.connect(self.close_application)
 
-        '''Adds toolbar and the action '''
+        ''' Adds toolbar and the action '''
         self.toolBar = self.addToolBar("Extraction")
         self.toolBar.addAction(extractAction)
 
-        '''Adding check box Functionality'''
+        ''' Adding check box Functionality '''
 
         checkBox = QtGui.QCheckBox('Enlarge Window',self)
         checkBox.toggle()
         checkBox.stateChanged.connect(self.enlargeWindow)
         checkBox.move(100,25)
 
+        '''Add Progress Bar '''
+        self.progress = QtGui.QProgressBar(self)
+        self.progress.setGeometry(200,80,250,20)
+
+        self.btn = QtGui.QPushButton("Download",self)
+        self.btn.move(200,120)
+        self.btn.clicked.connect(self.download);
+
+
         self.show()
+
+    def download(self):
+        self.completed = 0;
+        while self.completed < 100:
+            self.completed += 0.001
+            self.progress.setValue(self.completed)
 
     def enlargeWindow(self,state):
         if state == QtCore.Qt.Checked:
--- speech_to_text.py
+++ speech_to_text.py
@@ -6,7 +6,7 @@ with sr.Microphone() as source:
     print("Say something!")
     audio = r.listen(source)
 
-"""
+
 # recognize speech using Google Speech Recognition
 try:
     # for testing purposes, we're just using the default API key
@@ -17,4 +17,3 @@ except sr.UnknownValueError:
     print("Google Speech Recognition could not understand audio")
 except sr.RequestError as e:
     print("Could not request results from Google Speech Recognition service; {0}".format(e))
-"""
